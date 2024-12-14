0a67a937e8436f789249f98cb6541caf23cd7a4d
GintokiSa84 <dulygin18@gmail.com>
2024-12-14 19:09:47 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+from string import printable
+print("1st program")
+a=(9**0.5*5)
+print('9**0.5*5=a')
+
+print("2st program")
+print(('9.99>9.98 and 1000!=1000.1'),9.99>9.98 and 1000!=1000.1)
+
+print("3st program")
+a=(2*2+2)
+b=(2*(2+2))
+print(a==b)
+
+print("4st program")
+a=((float("123.456"))*10%10)
+print(int(a))
